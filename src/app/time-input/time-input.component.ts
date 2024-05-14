722556088a6dc9ae68659582500a27d21b88c610
eh1160 <eric@broomtreeweb.com>
2024-05-14 16:52:20 -0400

    Time input handle no space before am/pm, and... * Shouldn't show error when blank

--- src/app/time-input/time-input.component.ts
+++ src/app/time-input/time-input.component.ts
@@ -10,16 +10,21 @@ import { DateTime } from 'luxon';
   templateUrl: './time-input.component.html',
   styleUrl: './time-input.component.scss'
 })
+
 export class TimeInputComponent implements OnInit {
   inputDateTime: string = '8:30:00 AM';
   outputDateTime: string | null = null;
-  isValid: boolean = false;
+  isValid: boolean | undefined = false;
+
+  // https://moment.github.io/luxon/#/formatting?id=table-of-tokens
   validTimeFormats: Set<string> = new Set([
     'h:mm', // defaults to AM
-    'h:mm a', // includes AM/PM
+    'h:mma', // includes AM/PM (no space)
+    'h:mm a', // includes AM/PM (with space)
     'H:mm', // 24 hour
 
     'h:mm:ss',
+    'h:mm:ssa',
     'h:mm:ss a',
     'H:mm:ss',
   ]);
@@ -28,24 +33,35 @@ export class TimeInputComponent implements OnInit {
   invalidExplanation: string | null = null;
 
   validateTime(inputTime: string): void {
-    for (const timeFormat of this.validTimeFormats) {
-      const givenDateTime: DateTime = DateTime.fromFormat(
-        inputTime.trim(),
-        timeFormat
-      );
+    // Military time shouldn't accept AM/PM.  Why is it?  See https://github.com/moment/luxon/issues/1625
+    // console.log('*************', DateTime.fromFormat('18:30 AM', 'h:mm a').isValid);
 
-      if (givenDateTime.isValid) {
-        this.outputDateTime = givenDateTime.toISO();
-        this.isValid = true;
-        this.invalidReason = null;
-        this.invalidExplanation = null;
-        break;
-      } else {
-        this.outputDateTime = null;
-        this.isValid = false;
-        this.invalidReason = givenDateTime.invalidReason;
-        this.invalidExplanation = givenDateTime.invalidExplanation;
+    if (inputTime) {
+      for (const timeFormat of this.validTimeFormats) {
+        const givenDateTime: DateTime = DateTime.fromFormat(
+          inputTime.trim(),
+          timeFormat
+        );
+
+        if (givenDateTime.isValid) {
+          this.outputDateTime = givenDateTime.toISOTime();
+          this.isValid = true;
+          this.invalidReason = null;
+          this.invalidExplanation = null;
+          break;
+        } else {
+          this.outputDateTime = null;
+          this.isValid = false;
+          this.invalidReason = givenDateTime.invalidReason;
+          this.invalidExplanation = givenDateTime.invalidExplanation;
+        }
       }
+    } else {
+      // empty input
+      this.outputDateTime = null;
+      this.isValid = undefined;
+      this.invalidReason = null;
+      this.invalidExplanation = null;
     }
   }
 
